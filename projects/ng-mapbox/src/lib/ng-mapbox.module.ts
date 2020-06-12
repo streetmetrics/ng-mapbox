ec672b48c0b06695e0fc61379a539786d2e32ede
gvonhagel <greyson@streetmetrics.io>
2020-06-12 13:25:58 -0500

    - Added Component exports in core NgModule - Upgraded mapbox-gl

--- projects/ng-mapbox/src/lib/ng-mapbox.module.ts
+++ projects/ng-mapbox/src/lib/ng-mapbox.module.ts
@@ -3,10 +3,22 @@ import { MapboxOptions } from 'mapbox-gl';
 import { GLOBAL_MAP_OPTIONS } from './constants';
 import { MapComponent } from './map/map.component';
 import { LayerComponent } from './layer/layer.component';
+import { MarkerComponent } from './marker/marker.component';
+import { PopupDirective } from './marker/popup.directive';
+import { ImageComponent } from './image/image.component';
+import { VectorSourceComponent } from './source/vector.component';
+import { GeoJSONComponent } from './source/geojson.component';
+import { BoundsComponent } from './bounds/bounds.component';
 
 const EXPORT_COMPONENTS = [
   MapComponent,
+  BoundsComponent,
+  GeoJSONComponent,
+  ImageComponent,
   LayerComponent,
+  MarkerComponent,
+  PopupDirective,
+  VectorSourceComponent,
 ];
 
 @NgModule({
