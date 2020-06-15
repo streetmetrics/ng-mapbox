36c3b021ceaa38923ea18e4006016b897857883f
gvonhagel <greyson@streetmetrics.io>
2020-06-15 14:40:07 -0500

    public-api.ts should export Navigation/Geolocator control classes

--- projects/ng-mapbox/src/public-api.ts
+++ projects/ng-mapbox/src/public-api.ts
@@ -5,7 +5,10 @@ export * from './lib/ng-mapbox.module';
 
 export * from './lib/bounds/bounds.component';
 
+export * from './lib/control/geolocator/geolocator.control';
 export * from './lib/control/geolocator/geolocator.component';
+
+export * from './lib/control/navigation/navigation.control';
 export * from './lib/control/navigation/navigation.component';
 
 export * from './lib/image/image.component';
